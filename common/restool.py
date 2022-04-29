2a1da67155895d5d704d5b1510bbe8ee07a35f29
Yii-xiii <yinxuan99@outlook.com>
2022-04-29 03:11:19 -0700

    changed all http-status-code to 200

--- common/restool.py
+++ common/restool.py
@@ -4,38 +4,38 @@ def resOk(message=""):
     return JsonResponse({'code' : 200, "message" : message})
 
 def resError(code, message=""):
-    return JsonResponse({'code' : code, "message" : message}, status=code)
+    return JsonResponse({'code' : code, "message" : message})
 
 def resMissingPara(list):
     if len(list) == 1:
-        return JsonResponse({'code' : 400, "message" : str(list) + " is required."}, status=400)    
-    return JsonResponse({'code' : 400, "message" : str(list) + " are required."}, status=400)
+        return JsonResponse({'code' : 400, "message" : str(list) + " is required."})    
+    return JsonResponse({'code' : 400, "message" : str(list) + " are required."})
 
 def resReturn(data):
     return JsonResponse({'code' : 200, **data})
 
 def resInvalidPara(list:str):
     if len(list) == 1:
-        return JsonResponse({'code' : 400, "message" : "Invalid " + list[0] + "."}, status=400)   
+        return JsonResponse({'code' : 400, "message" : "Invalid " + list[0] + "."})   
 
     s = ""
     for l in list[:-1]:
          s += l + ", "
     s += "or " + list[-1]
 
-    return JsonResponse({'code' : 400, "message" : "Invalid " + s + "."}, status=400)
+    return JsonResponse({'code' : 400, "message" : "Invalid " + s + "."})
 
 def resForbidden(message=""):
-    return JsonResponse({'code' : 403, "message" : message}, status=403)
+    return JsonResponse({'code' : 403, "message" : message})
 
 def resUnauthorized(message=""):
-    return JsonResponse({'code' : 401, "message" : message}, status=401)
+    return JsonResponse({'code' : 401, "message" : message})
 
 def resNotFound(message=""):
-    return JsonResponse({'code' : 404, "message" : message}, status=404)
+    return JsonResponse({'code' : 404, "message" : message})
 
 def resBadRequest(message=""):
-    return JsonResponse({'code' : 400, "message" : message}, status=400)
+    return JsonResponse({'code' : 400, "message" : message})
 
 def resFile(file):
     return FileResponse(file)
