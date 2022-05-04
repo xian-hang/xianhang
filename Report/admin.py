d6f5eb783d903a031c624fc9beb8ea6046aa7b35
Yii-xiii <yinxuan99@outlook.com>
2022-05-04 00:09:18 -0700

    added ReportNotice in admin site

--- Report/admin.py
+++ Report/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Report
+from .models import Report, ReportNotice
 
 class ReportAdmin(admin.ModelAdmin):
     list_display = ('user','reporting','status')
 
+class ReportNoticeAdmin(admin.ModelAdmin):
+    list_display = ('report','content')
+
+
 # Register your models here.
 admin.site.register(Report, ReportAdmin)
+admin.site.register(ReportNotice, ReportNoticeAdmin)
