d8c952080b76a6cecc87143041c26fe83f362e18
Alvintan0712 <tjan2000@gmail.com>
2022-05-11 14:30:20 +0800

    add cors

--- xianhang/settings.py
+++ xianhang/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     'XHUser',
     'Product',
     'Collection',
@@ -58,10 +59,16 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://xianhang.ga', 
 ]
 
 ROOT_URLCONF = 'xianhang.urls'
