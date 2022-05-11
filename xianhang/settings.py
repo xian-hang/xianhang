11aaba230106af67e0c4bbc01e9ad1a5c1fdba5e
EC2 Default User <ec2-user@ip-172-31-17-88.ec2.internal>
2022-05-11 06:28:26 +0000

    add domain

--- xianhang/settings.py
+++ xianhang/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-gu$3um!!l-1cw5nj2js4kj!6ha&hr^=0l4b!q9gb%w^pnqo#9v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.0.117','localhost','xianhang.herokuapp.com','xianhang-web.herokuapp.com', '127.0.0.1', '139.162.90.35', 'r.idiv.cf','u.idiv.cf', 'x.cirno.ga', '18.205.90.245']
+ALLOWED_HOSTS = ['192.168.0.117','localhost','xianhang.herokuapp.com','xianhang-web.herokuapp.com', '127.0.0.1', '139.162.90.35', 'r.idiv.cf','u.idiv.cf', 'x.cirno.ga', '18.205.90.245', 'xianhang.ga']
 
 # Application definition
 
