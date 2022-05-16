a3128036ffe2ae89972dcc00f461ed3fbc7f28f3
Yii-xiii <yinxuan99@outlook.com>
2022-05-15 22:31:29 -0700

    changed to case insensitive in searchUser

--- XHUser/views.py
+++ XHUser/views.py
@@ -362,8 +362,8 @@ def searchUser(request):
     if not keywordValidation(keyword):
         return resInvalidPara(["keyword"])
 
-    users = set(XHUser.objects.filter(username__contains=keyword, role=XHUser.RoleChoice.USER, status__in=[XHUser.StatChoice.VER, XHUser.StatChoice.RESTRT]))
-    users |= set(XHUser.objects.filter(studentId__contains=keyword, role=XHUser.RoleChoice.USER, status__in=[XHUser.StatChoice.VER, XHUser.StatChoice.RESTRT]))
+    users = set(XHUser.objects.filter(username__icontains=keyword, role=XHUser.RoleChoice.USER, status__in=[XHUser.StatChoice.VER, XHUser.StatChoice.RESTRT]))
+    users |= set(XHUser.objects.filter(studentId__icontains=keyword, role=XHUser.RoleChoice.USER, status__in=[XHUser.StatChoice.VER, XHUser.StatChoice.RESTRT]))
     if getReqUser(request) is not None:
         users -= {getReqUser(request)}
 
