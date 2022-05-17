5ebfce0fd252c5cf6089dc45ed54640dd6fafc31
Yii-xiii <yinxuan99@outlook.com>
2022-05-17 01:57:44 -0700

    changed room_name to userId

--- Chat/urls.py
+++ Chat/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<str:room_name>/', views.room, name='room'),
+    path('login/', views.loginTest),
+    path('<str:userId>/', views.room),
 ]
