7f91ad01ebcabf475ce77cb83cc6135d340a2a16
Yii-xiii <yinxuan99@outlook.com>
2022-05-17 01:58:28 -0700

    added Chat and Message models

--- Chat/models.py
+++ Chat/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from XHUser.models import XHUser
+import json
 
 # Create your models here.
+class Chat(models.Model):
+    users = models.ManyToManyField(XHUser)
+
+class Message(models.Model):
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE,null=True)
+    author = models.ForeignKey(XHUser, on_delete=models.CASCADE)
+    content = models.CharField(max_length=150)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def body(self):
+        return {
+            'author' : self.author.username,
+            'message' : self.content,
+            'time' : self.timestamp.isoformat(),
+        }
