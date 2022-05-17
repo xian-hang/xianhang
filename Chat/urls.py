2256a34f9494359535d4e880e9bb5db50986c229
Yii-xiii <yinxuan99@outlook.com>
2022-05-17 03:36:25 -0700

    added getChatList

--- Chat/urls.py
+++ Chat/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.loginTest),
-    path('<str:userId>/', views.room),
+    path('list/', views.getChatList),
+    # path('<str:userId>/', views.room),
 ]
--- Chat/views.py
+++ Chat/views.py
@@ -1,6 +1,6 @@
-from email import message
+
 from django.shortcuts import render
-from Chat.models import Chat
+from Chat.models import Chat, Message
 from django.contrib.auth import login
 
 from common.deco import user_logged_in
@@ -8,6 +8,8 @@ from common.functool import getActiveUser, getReqUser, getUser
 from common.restool import resNotFound, resOk
 
 # Create your views here.
+def messageTime(ele):
+    return ele['lastMessage']['time']
 
 def loginTest(request):
     user=getUser(id=7)
@@ -42,3 +44,17 @@ def getChat(request, userId):
 
     return resOk({'id' : chat.id, 'username' : user.username, 'messages' : [m.body() for m in messages]})
 
+@user_logged_in
+def getChatList(request):
+    reqUser = getReqUser(request)
+    chats = reqUser.chat_set.all()
+
+    results=[]
+    for c in chats:
+        messages = Message.objects.filter(chat=c).order_by('-timestamp')
+        if len(messages):
+            user = c.users.exclude(id=reqUser.id).first()
+            results += [{'chatId' : c.id, 'lastMessage' : messages[0].body(), 'username' : user.username}]
+    
+    results.sort(key=messageTime)
+    return resOk({'result' : results})
