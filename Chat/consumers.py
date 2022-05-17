d4bd7a121257cbcf2b38dbf218aba8d9c97b720d
Yii-xiii <yinxuan99@outlook.com>
2022-05-17 03:38:07 -0700

    added commandTypes and changed message_id to channel_id

--- Chat/consumers.py
+++ Chat/consumers.py
@@ -1,5 +1,6 @@
 
 import json
+
 from Chat.models import Message, Chat
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
@@ -25,9 +26,55 @@ def getChat(user1,user2):
         return chat.pop()
 
 class ChatConsumer(WebsocketConsumer):
-    NEW_MESS = 'newMessage'
-    NEW_CHAT = 'newChat'
-    FET_MESS = 'fetchMessage'
+
+    #command func
+    def newMessage(self, data):
+
+        user = getUser(id = data['userId'])
+        reqUser = getReqUser(self.scope)
+
+        chat = getChat(user,reqUser)
+        if chat is None:
+            chat = Chat.objects.create()
+            chat.users.add(user, reqUser)
+
+            async_to_sync(self.channel_layer.group_send)(
+                'channel_%s' % user.id,
+                {
+                    'type' : 'newChat',
+                    'chatId' : chat.id,
+                    'message' : 'add new chat'
+                }
+            )
+
+            async_to_sync(self.channel_layer.group_add)(
+                'chat_%s' % chat.id,
+                self.channel_name
+            )
+        
+        message = Message.objects.create(message=data['message'], author= reqUser,chat=chat)
+        async_to_sync(self.channel_layer.group_send)(
+            'chat_%s' % chat.id,
+            {
+                'type' : 'sendMessage',
+                **message.body(),
+            }
+        )
+
+    def fetchMessage(self, data):
+        userId = data['userId']
+        user = getUser(id=userId)
+        reqUser = getReqUser(self.scope)
+        chat = getChat(user, reqUser)
+
+        messages = Message.objects.filter(chat=chat)
+        self.send(text_data=json.dumps({'message' : [m.body() for m in messages]}))
+
+    # command type
+    commandTypes = {
+        'newMessage' : newMessage,
+        'fetchMessage' : fetchMessage,
+    }
 
     def connect(self):
         reqUser = getReqUser(self.scope)
@@ -35,7 +82,7 @@ class ChatConsumer(WebsocketConsumer):
             # print('connect : ', self.scope)
 
             async_to_sync(self.channel_layer.group_add)(
-                'message_%s' % reqUser.id,
+                'channel_%s' % reqUser.id,
                 self.channel_name
             )
 
@@ -54,7 +101,7 @@ class ChatConsumer(WebsocketConsumer):
         self.send(text_data=json.dumps({'message' : 'disconnect successfully'}))
 
         async_to_sync(self.channel_layer.group_discard)(
-            'message_%s' % reqUser.id,
+            'channel_%s' % reqUser.id,
             self.channel_name
         )
 
@@ -70,44 +117,10 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data=None):
         # print('receive : ', self.scope)
         data = json.loads(text_data)
-        if data['type'] == ChatConsumer.NEW_MESS:
-            self.newMessage(data)
-        if data['type'] == ChatConsumer.FET_MESS:
-            self.fetchMessage(data)
+        self.commandTypes[data['type']](self, data)
 
-    def newMessage(self, data):
 
-        user = getUser(id = data['userId'])
-        reqUser = getReqUser(self.scope)
-
-        chat = getChat(user,reqUser)
-        if chat is None:
-            chat = Chat.objects.create()
-            chat.users.add(user, reqUser)
-
-            async_to_sync(self.channel_layer.group_send)(
-                'message_%s' % user.id,
-                {
-                    'type' : 'newChat',
-                    'chatId' : chat.id,
-                    'message' : 'add new chat'
-                }
-            )
-
-            async_to_sync(self.channel_layer.group_add)(
-                'chat_%s' % chat.id,
-                self.channel_name
-            )
-        
-        Message.objects.create(message=data['message'], author= reqUser,chat=chat)
-        async_to_sync(self.channel_layer.group_send)(
-            'chat_%s' % chat.id,
-            {
-                'type' : 'sendMessage',
-                'message' : {'message' : 'talking to ' + data['receiver'] + ' : ' + data['message']}
-            }
-        )
-
+    # group command type func
     def newChat(self, data):
         chatId = data['chatId']
         async_to_sync(self.channel_layer.group_add)(
@@ -121,12 +134,3 @@ class ChatConsumer(WebsocketConsumer):
     def sendMessage(self, data):
         message = data['message']
         self.send(text_data=json.dumps(message))
-
-    def fetchMessage(self, data):
-        userId = data['userId']
-        user = getUser(id=userId)
-        reqUser = getReqUser(self.scope)
-        chat = getChat(user, reqUser)
-
-        messages = Message.objects.filter(chat=chat)
-        self.send(text_data=json.dumps({'message' : [m.body() for m in messages]}))
