da3f1c810524e81915f313b53d9896d062354d41
Yii-xiii <yinxuan99@outlook.com>
2022-05-18 00:02:19 -0700

    changed resOk to resReturn in getChatList

--- Chat/views.py
+++ Chat/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth import login
 
 from common.deco import user_logged_in
 from common.functool import getActiveUser, getReqUser, getUser
-from common.restool import resNotFound, resOk
+from common.restool import resNotFound, resOk, resReturn
 
 # Create your views here.
 def messageTime(ele):
@@ -42,7 +42,7 @@ def getChat(request, userId):
 
     messages = chat.messages_set.all()
 
-    return resOk({'id' : chat.id, 'username' : user.username, 'messages' : [m.body() for m in messages]})
+    return resReturn({'id' : chat.id, 'username' : user.username, 'messages' : [m.body() for m in messages]})
 
 @user_logged_in
 def getChatList(request):
@@ -57,4 +57,4 @@ def getChatList(request):
             results += [{'chatId' : c.id, 'lastMessage' : messages[0].body(), 'username' : user.username, 'userId' : user.id}]
     
     results.sort(key=messageTime)
-    return resOk({'result' : results})
+    return resReturn({'result' : results})
